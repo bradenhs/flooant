7e9fcffed9a95b20686cad3bd40304d6ac0d34e9
Braden Snell <braden@flooant.com>
2018-03-31 00:10:22 -0400

    adds in server code

--- app/main/App.tsx
+++ app/main/App.tsx
@@ -14,7 +14,7 @@ export class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#ff9",
     alignItems: "center",
     justifyContent: "center"
   }
